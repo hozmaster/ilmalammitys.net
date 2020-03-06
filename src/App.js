714ada996bdc12222197e3e4776142c57e53649f
Olli-Pekka Wallin <opwallin@gmail.com>
2020-03-06 18:55:12 +0200

    First version from working pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+
+import { Button } from 'semantic-ui-react';
+
 import logo from './logo.svg';
-import 'semantic-ui-css/semantic.min.css'
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-            </header>
-            <p>Test</p>>
+            <div className="ui raised very padded text container segment">
+                <h2 className="ui header">Dogs Roles with Humans</h2>
+                <p>Domestic dogs inherited complex behaviors, such as bite inhibition, from their wolf ancestors, which would have been pack hunters with complex body language. These sophisticated forms of social cognition and communication may account for their trainability, playfulness, and ability to fit into human households and social situations, and these attributes have given dogs a relationship with humans that has enabled them to become one of the most successful species on the planet today.</p>
+                <p>The dogs' value to early human hunter-gatherers led to them quickly becoming ubiquitous across world cultures. Dogs perform many roles for people, such as hunting, herding, pulling loads, protection, assisting police and military, companionship, and, more recently, aiding handicapped individuals. This impact on human society has given them the nickname "man's best friend" in the Western world. In some cultures, however, dogs are also a source of meat.</p>
+            </div>
         </div>
     );
 }
