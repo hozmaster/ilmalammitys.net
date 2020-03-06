d7144fc865864be9c4ba2fdb7c05723ecf02cea4
Olli-Pekka Wallin <opwallin@gmail.com>
2020-03-06 22:08:37 +0200

    Added Footer and Feedback pages and components

--- src/App.js
+++ src/App.js
@@ -10,24 +10,28 @@ import {
 import Home from "./Pages/Home";
 import Technic from "./Pages/Technic";
 import Fileslistings from "./Pages/Fileslistings"
-
+import Feedback from "./Pages/Feedback";
+import Footer from "./Components/Footer";
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <div className="ui raised very padded text container segment">
-                    <div className="ui three item menu">
+                    <div className="ui five item menu">
                         <a href={"/"}className="active item">Home</a>
                         <a href={'/tekniikka'} className="item">Tekniikka</a>
                         <a href={'/tiedostot'} className="item">Tiedostot</a>
+                        <a href={'/tiedostot'} className="item">Tiedostot</a>
+                        <a href={'/palaute'} className="item">Palaute</a>
                     </div>
                     <Route exact path='/' component={Home}/>
                     <Route path='/tekniikka' component={Technic}/>
                     <Route path='/tiedostot' component={Fileslistings}/>
+                    <Route path='/palaute' component={Feedback}/>
+                    <Footer/>
                 </div>
             </div>
-
         </Router>
     );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020.  Olli-Pekka Wallin All rights reserved
+ */
+
+
+import React from 'react'
+
+const Footer = () =>
+    <div className="ui message center aligned" >
+        mail to: &nbsp;opwallin (at) gmail.com
+    </div>
+export default Footer
+
+
