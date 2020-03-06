a2cc42c5d3852a5cb7341034a7bee5e0b28667b5
Olli-Pekka Wallin <opwallin@gmail.com>
2020-03-06 21:24:00 +0200

    Add tow basic test pages

---
+++ src/Pages/Technic.jsx
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020.  Olli-Pekka Wallin All rights reserved
+ */
+
+import React from 'react'
+
+const Technic = () => <div>
+Varsinaisen lämmityksen lisäksi tarvitaan siis lämmönjakokeskus joka huolehtii lämpimän veden lämmityksestä ja joka välittää kulloinkin ajankohtaan sopivan määrän kaukolämpöä ilmalämmityslaitteen vesipatteristoon. Suomessa lämmönjakokeskus laitteiden valmistajia on jonkun verran markkinoilla. Ilmalämmistystaloissa jotka on liitetty kaukolämpöverkostoon, lämmönjakokeskuksen tyyppi tulisi olla vähintään ns. 2-piirinen.
+    Tällöin lämmönsääätöä voidaan tehdä erikseen lämpimälle vedelle ilman että se häiritsee varsinaista huoneiston lämmitystä.
+</div>
+
+export default Technic
