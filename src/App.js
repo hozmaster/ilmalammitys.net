edf583bfa86c4fa56a8e1b4772df5d8bdee61434
Olli-Pekka Wallin <opwallin@gmail.com>
2020-03-06 21:48:24 +0200

    Add new files starting point

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
+
 import React from 'react';
 
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+
 import Home from "./Pages/Home";
+import Technic from "./Pages/Technic";
+import Fileslistings from "./Pages/Fileslistings"
+
 
 function App() {
     return (
-        <div className="App">
-            <div className="ui raised very padded text container segment">
-                <div className="ui three item menu">
-                    <a className="active item">Editorials</a>
-                    <a className="item">Reviews</a>
-                    <a className="item">Upcoming Events</a>
+        <Router>
+            <div className="App">
+                <div className="ui raised very padded text container segment">
+                    <div className="ui three item menu">
+                        <a href={"/"}className="active item">Home</a>
+                        <a href={'/tekniikka'} className="item">Tekniikka</a>
+                        <a href={'/tiedostot'} className="item">Tiedostot</a>
+                    </div>
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/tekniikka' component={Technic}/>
+                    <Route path='/tiedostot' component={Fileslistings}/>
                 </div>
-                <Home/>
             </div>
-        </div>
+
+        </Router>
     );
 }
 
