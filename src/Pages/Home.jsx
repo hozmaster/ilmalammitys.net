6f1c33cd47757a7dba397e315cc7e82ed33ec94e
Olli-Pekka Wallin <opwallin@gmail.com>
2020-03-07 23:36:15 +0200

    Add more text to pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,9 +5,20 @@
 import React from 'react'
 
 const Home = () => <div>
-    <h2 className="ui header">Dogs Roles with Humans</h2>
+
+    <p> </p>
+    <h3 className="ui header">Ilmalämmitys lyhyesti </h3>
     <p>Ilmalämmityksessä huoneisto lämmitetään lämpimän ilman avulla. Lämmitetty ilma tuodaan huoneisiin ilmakanavia pitkin huoneisiin. Ilmakanavat sijaitsevat lattiassa sokkelissa taikka talon rakenteissa. Ilma jaetaan huoneistoihin lattian rajassa olevien ritilöiden avulla.</p>
-    <p>Domestic dogs inherited complex behaviors, such as bite inhibition, from their wolf ancestors, which would have been pack hunters with complex body language. These sophisticated forms of social cognition and communication may account for their trainability, playfulness, and ability to fit into human households and social situations, and these attributes have given dogs a relationship with humans that has enabled them to become one of the most successful species on the planet today.</p>
-    <p>The dogs' value to early human hunter-gatherers led to them quickly becoming ubiquitous across world cultures. Dogs perform many roles for people, such as hunting, herding, pulling loads, protection, assisting police and military, companionship, and, more recently, aiding handicapped individuals. This impact on human society has given them the nickname "man's best friend" in the Western world. In some cultures, however, dogs are also a source of meat.</p>
-</div>
+    <p>Tässä sivuilla keskitytään siihen mitä laitteita ja eri osa-alueita tarvitaan lämmityksen ja lämpimän veden tuottamiseen tavallisessa omakotitalossa joka on liitetty kaukolämpöverkkoon sekä jonka lämmitys hoidetaan ilmalämmityksen avulla. </p>
+    <p>Aika on rajallinen määre joka pysyy samana ja on minulla täyteen ahdettu. Siksi en tarjoa apua taikka tukea puhelimitse. Toki vastaan sähköpostiin kun ehdin. </p>
+
+    <h4>Yleistä</h4>
+    
+    <p>Ilmalämmitys on oikein suunniteltuna riittävä lämmityksen lähde  normaalikokoisille omakotitalolle Suomessa (oman kokemuksen mukaan). Joskus olemme lämmittäneet talossamme olevaa leivinuunia esimerkiksi kovilla pakkasilla.</p>
+    <p>80-luvun  valmistetut laitteistot ovat pian käyttöiän päässä, joten näiden laitteistojen uusiminen/saneeraaminen on ajankohtainen tällä hetkellä. Varaosia ainakin Valmetin Kotilämpölaitteistoon on saatavilla nopeastikin.</p>
+    <p>Tällä hetkellä markkinoilla on muutamia valmistajia jotka valmistavat laitteita yksityissektorille. Nämä laitteistot on tarkoitetu lähinnä saneerauskohteisiin mutta esteitä uudisrakentamisen ei pitäisi olla. Iilmälämmitysmarkkinointi ja laitteiden valmistus on suunnattu tällä hetkellä yrityskiinteistöihin kuten puutarhatilojen lämmitykseen sekä julkisiin rakennuksiin.</p>
+    <p>Jos sinulla on kaaviokuvia taikka teknisiä tietoja kuten sähkö-ja puhallustietoja koskien erityisesti Valmetin kotilämpölaitteistoa mallia 1000, niin otan mielellään vastaan. Samalla otan vastaan kommenteja tästä sivustosta taikka tietoja muiden laitteiden ilmalämmityksestä laitetavaksi näille sivuille. </p>
+
+</div>;
 export default Home
+
--- src/Pages/Technic.jsx
+++ src/Pages/Technic.jsx
@@ -5,8 +5,38 @@
 import React from 'react'
 
 const Technic = () => <div>
-Varsinaisen lämmityksen lisäksi tarvitaan siis lämmönjakokeskus joka huolehtii lämpimän veden lämmityksestä ja joka välittää kulloinkin ajankohtaan sopivan määrän kaukolämpöä ilmalämmityslaitteen vesipatteristoon. Suomessa lämmönjakokeskus laitteiden valmistajia on jonkun verran markkinoilla. Ilmalämmistystaloissa jotka on liitetty kaukolämpöverkostoon, lämmönjakokeskuksen tyyppi tulisi olla vähintään ns. 2-piirinen.
-    Tällöin lämmönsääätöä voidaan tehdä erikseen lämpimälle vedelle ilman että se häiritsee varsinaista huoneiston lämmitystä.
-</div>
+
+    <h4> Lämmönjakeskus </h4>
+    <p>Varsinaisen lämmityksen lisäksi tarvitaan siis lämmönjakokeskus joka huolehtii lämpimän veden lämmityksestä ja joka välittää kulloinkin ajankohtaan sopivan määrän kaukolämpöä ilmalämmityslaitteen vesipatteristoon. Suomessa lämmönjakokeskus laitteiden valmistajia on jonkun verran markkinoilla. Ilmalämmistystaloissa jotka on liitetty kaukolämpöverkostoon, lämmönjakokeskuksen tyyppi tulisi olla vähintään ns. 2-piirinen. </p>
+    <p>Tällöin lämmönsääätöä voidaan tehdä erikseen lämpimälle vedelle ilman että se häiritsee varsinaista huoneiston lämmitystä.</p>
+
+    <h4> Ilmalämmityskone </h4>
+
+    <p>Lämminilmakone jakaantuu karkeasti ottaen kolmeen osaan: LTO, puhaltimiin ja itse lämpimän ilman välittämiseen suunniteltuun puhaltimeen sekä kennostoon.</p>
+
+    <h5>Puhaltimet:</h5>
+
+    <p>Laitteessa on siis ainakin kolme erilaista puhallinta: poistoilmanpuhallin, raitisilmapuhallin ja varsinainen lämminilmanpuhallin joka puhaltaa lämmitettävän ilman lämmityspatteriston lävitse talon ilmakanavavistoon.></p>
+
+    <h5>Raitisilmapuhallin</h5>
+
+    <p>Raitisilmapuhallin sijaitsee siis LTO kennon alapuolella laitteiston ovesta katsottuna vasemmalla. Tämä puhallin on selvästi pienempi kuin poistoilmapuhallin, ja tarvitsee 2mikroF kondesaattorin (siis Vallox:in mukaan, ainakaan meillä ei kodensaattoria asennettu). Käynnistyy yhtäaikasesti poistoilmapuhaltimen kanssa.</p>
+
+    <h5>Poistopuhallin</h5>
+
+    <p>Poistoilmapuhallin sijaitsee TO kennostosta katsottuna oikealla puolella.</p>
+
+    <p>Puhaltimesta käy Ebmpast, mallinumero  D2E 133-AM47-01. Puhaltimen mukana pitäisi tulla 3mikroF kondesaattori (jota itse asiassa ei asenneta vaan se jää hyllyn koristeeksi.) Asennus mielellään osaavan LVI asentajan toimesta, kätisyys voidaan muuttaa laitteelle sopivaksi. Kysy Vallox:ilta taikka LVI myymälästä.</p>
+
+    <h4>Lämminilmapuhallin</h4>
+    <p>Lämminilmapuhallin sijaitsee  koneiston alaosassa, lämmityspatteriston yläpuolellla.</p>
+
+    <h3>Lämminilmapuhallin</h3>
+    <p> Lämmin ilma tuodaan taloon ilmakanavia pitkin jotka on upotettu yksikerroksisessa talossa sokkeliin valun yhteydessä. Nämä ilmakanavat tulisi puhdistaa ainakin kerran 15 vuodessa asiaan erikoistuneen huoltoliikkeen toimesta. Jotkut nuohoojat tarjoavat myöskin kanaviston puhdistuspalveluita joten kannattaa kysellä hintoja sekä mahdollisuutta puhdistuksiin myöskin alueesi nuohoojilta.</p>
+    <p>  </p>
+    <p> Karkeasti voidaan sanoa että erikoisfirmojen palveluita voidaan käyttää ympäri vuoden ja nuohoojien palveluita kesäaikaan.</p>
+
+    <h4></h4>
+</div>;
 
 export default Technic
