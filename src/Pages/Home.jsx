b450236278c5044e000da094fc0a4760e2588b6f
Olli-Pekka Wallin <opwallin@gmail.com>
2020-03-14 00:48:34 +0200

    Change to Container blocks

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,22 +3,28 @@
  */
 
 import React from 'react'
+import {Container} from "semantic-ui-react";
 
 const Home = () => <div>
     <p> </p>
     <h3 className="ui header">Ilmalämmitys lyhyesti </h3>
-    <p>Ilmalämmityksessä huoneisto lämmitetään lämpimän ilman avulla. Lämmitetty ilma tuodaan huoneisiin ilmakanavia pitkin huoneisiin. Ilmakanavat sijaitsevat lattiassa sokkelissa taikka talon rakenteissa. Ilma jaetaan huoneistoihin lattian rajassa olevien ritilöiden avulla.</p>
-    <p>Tässä sivuilla keskitytään siihen mitä laitteita ja eri osa-alueita tarvitaan lämmityksen ja lämpimän veden tuottamiseen tavallisessa omakotitalossa joka on liitetty kaukolämpöverkkoon sekä jonka lämmitys hoidetaan ilmalämmityksen avulla. </p>
-    <p>Aika on rajallinen määre joka pysyy samana ja on minulla täyteen ahdettu. Siksi en tarjoa apua taikka tukea puhelimitse. Toki vastaan sähköpostiin kun ehdin. </p>
+    <Container textAlign={"justified"}>Ilmalämmityksessä huoneisto lämmitetään lämpimän ilman avulla. Lämmitetty ilma tuodaan huoneisiin ilmakanavia pitkin huoneisiin. Ilmakanavat sijaitsevat lattiassa sokkelissa taikka talon rakenteissa. Ilma jaetaan huoneistoihin lattian rajassa olevien ritilöiden avulla.</Container>
+    <p> </p>
+    <Container textAlign={"justified"}>Tällä sivuilla keskitytään siihen mitä laitteita ja eri osa-alueita tarvitaan lämmityksen ja lämpimän veden tuottamiseen tavallisessa omakotitalossa joka on liitetty kaukolämpöverkkoon sekä jonka lämmitys hoidetaan ilmalämmityksen avulla. </Container>
+    <p> </p>
+    <Container textAlign={"justified"}>Aika on rajallinen määre joka pysyy samana ja on minulla täyteen ahdettu. Siksi en tarjoa apua taikka tukea puhelimitse. Toki vastaan sähköpostiin kun ehdin. </Container>
+
 
     <h4>Yleistä</h4>
     
-    <p>Ilmalämmitys on oikein suunniteltuna riittävä lämmityksen lähde  normaalikokoisille omakotitalolle Suomessa (oman kokemuksen mukaan). Joskus olemme lämmittäneet talossamme olevaa leivinuunia esimerkiksi kovilla pakkasilla.</p>
-    <p>80-luvun  valmistetut laitteistot ovat pian käyttöiän päässä, joten näiden laitteistojen uusiminen/saneeraaminen on ajankohtainen tällä hetkellä. Varaosia ainakin Valmetin Kotilämpölaitteistoon on saatavilla nopeastikin.</p>
-    <p>Tällä hetkellä markkinoilla on muutamia valmistajia jotka valmistavat laitteita yksityissektorille. Nämä laitteistot on tarkoitetu lähinnä saneerauskohteisiin mutta esteitä uudisrakentamisen ei pitäisi olla. Iilmälämmitysmarkkinointi ja laitteiden valmistus on suunnattu tällä hetkellä yrityskiinteistöihin kuten puutarhatilojen lämmitykseen sekä julkisiin rakennuksiin.</p>
+    <Container textAlign={"justified"}>Ilmalämmitys on oikein suunniteltuna riittävä lämmityksen lähde  normaalikokoisille omakotitalolle Suomessa (oman kokemuksen mukaan). Joskus olemme lämmittäneet talossamme olevaa leivinuunia esimerkiksi kovilla pakkasilla.</Container>
+    <p> </p>
+    <Container textAlign={"justified"}>80-luvun  valmistetut laitteistot ovat pian käyttöiän päässä, joten näiden laitteistojen uusiminen/saneeraaminen on ajankohtainen tällä hetkellä. Varaosia ainakin Valmetin Kotilämpölaitteistoon on saatavilla nopeastikin.</Container>
+    <p> </p>
+    <Container textAlign={"justified"}>Tällä hetkellä markkinoilla on muutamia valmistajia jotka valmistavat laitteita yksityissektorille. Nämä laitteistot on tarkoitetu lähinnä saneerauskohteisiin mutta esteitä uudisrakentamisen ei pitäisi olla. Iilmälämmitysmarkkinointi ja laitteiden valmistus on suunnattu tällä hetkellä yrityskiinteistöihin kuten puutarhatilojen lämmitykseen sekä julkisiin rakennuksiin.</Container>
 
-    <h5>Muuta</h5>
-    <p>Jos sinulla on kaaviokuvia taikka teknisiä tietoja kuten sähkö-ja puhallustietoja koskien erityisesti Valmetin kotilämpölaitteistoa mallia 1000, niin otan mielellään vastaan. Samalla otan vastaan kommenteja tästä sivustosta taikka tietoja muiden laitteiden ilmalämmityksestä laitetavaksi näille sivuille. </p>
+    <h4>Muuta</h4>
+    <Container textAlign={"justified"}>Jos sinulla on kaaviokuvia taikka teknisiä tietoja kuten sähkö-ja puhallustietoja koskien erityisesti Valmetin kotilämpölaitteistoa mallia 1000, niin otan mielellään vastaan. Samalla otan vastaan kommenteja tästä sivustosta taikka tietoja muiden laitteiden ilmalämmityksestä laitetavaksi näille sivuille. </Container>
 
 </div>;
 export default Home
