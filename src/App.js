beb5e6f3eead8a3ee47a98ed57742de4364fffeb
Olli-Pekka Wallin <opwallin@gmail.com>
2020-03-29 23:10:33 +0300

    stuff

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Route,
+<<<<<<< Updated upstream
     Link
 } from 'react-router-dom'
+=======
+    Link,
+    NavLink,
+} from "react-router-dom";
+>>>>>>> Stashed changes
 
 import Home from "./Pages/Home";
 import Technic from "./Pages/Technic";
@@ -16,11 +22,13 @@ function App() {
     return (
         <Router>
             <div className="App">
-                <div className="ui raised very padded text container segment">
-                    <div className="ui three item menu">
-                        <a href={"/"}className="active item">Home</a>
-                        <a href={'/tekniikka'} className="item">Tekniikka</a>
-                        <a href={'/tiedostot'} className="item">Tiedostot</a>
+                <div className="ui raised very padded text container">
+                    <div className="ui five item menu">
+                        <NavLink exact to="/" activeClassName="active" className="item">Home</NavLink>{' '}
+                        <NavLink exact to="/tekniikka" activeClassName="active" className="item">Tekniikka</NavLink>{' '}
+                        <NavLink exact to={'/tiedostot'} activeClassName="active" className="item">Tiedostot</NavLink>{' '}
+                        <NavLink exact to={'/valmistajat'} activeClassName="active" className="item">Valmistajat</NavLink>{' '}
+                        <NavLink exact to={'/palaute'} activeClassName="active" className="item">Palaute</NavLink>{' '}
                     </div>
                     <Route exact path='/' component={Home}/>
                     <Route path='/tekniikka' component={Technic}/>
