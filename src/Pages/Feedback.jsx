bfa33a9d0f7c9db218dad3972befef72449bc7f5
Olli-Pekka Wallin <olli-pekka.wallin@wallin.biz>
2020-04-10 23:29:43 +0300

    Transform old header usage to semantic ui style

--- src/Pages/Feedback.jsx
+++ src/Pages/Feedback.jsx
@@ -7,10 +7,14 @@ import {Container, Header} from "semantic-ui-react";
 
 const Feedback = () => <div>
     <p> </p>
-    <Header as={"h3"} > Linkit </Header>
-    
-    <h3 className="ui header">Palaute</h3>
-    <h4>Nim. "ROT"</h4>
+    <Header as={"h2"} > Linkit </Header>
+
+    <p> link 1 </p>
+    <p> link 2 </p>
+    <p> link 3 </p>
+
+    <Header as={"h2"} >Palaute</Header>
+    <Header as={"h4"}> Nim. "ROT"</Header>
     <Container textAlign={"justified"}>Jo vuonna 1980 Hankkijan ja Puolimatkan Rakennusvalmiste Oy:n välillä oli läheistä yhteistyötä ja yksi ilmentymä oli PARMA-ilmalämmitysjärjestelmän markkinointi. Meille asennettiin ko. järjestelmä vuonna 1980 omakotitaloon.  Lämmönlähteenä oli 3000L Galvanoimis Oy:n lämminvesivaraaja. Järjestelmä on ollut sellaisenaan käytössä kesään 2014 saakka, jolloin varaaja korvattiin alkukesästä Thermian maalämpölaitteistolla. Tekstisi lopussa annat melko varovaisen suosituksen ilmalämmityksen riittävyydestä omakotitaloon.  Turhaa varovaisuutta. Jos talon eristykset ovat kunnossa ja talossa kohtuullinen määrä lämpöä varaavaa massaa pärjää ilmalämmitys erinomaisesti kovimmillakin pakkasilla. Ja mikä tärkeintä ilmalämmityksen energiankulutus on erittäin kilpailukykyinen.</Container>
     <p> </p>
     <Container textAlign={"justified"}>Sivuiltasi huomasin, että tämä Hankkija /Parma on (siis ei Parma 10) on itse asiassa sama laite kuin ILA-15. Ainoa ero on, että ovi on yksiosainen. Talomme on kellarikerroksinen 1½ kerroksinen omakotitalo pohjapinta-ala 12×8.4m, jolloin lämmitettävä tilavuus on n.800 m³. Parmassa on kiertoilmapuhaltimella 4 nopeutta. Alin nopeus on riittänyt kaikissa olosuhteissa (-30C°), kun patterilla kiertävä vesi on ollut 60C°. Koko ajatus lämmityksen tehostamisesta ilmankierrätystä lisäämällä tuntuu virheelliseltä. Jo kakkosnopeus Parmassa aiheutta ääntä ja vetoa liikaa, nelosella pyörii sukat jalassa. Minusta ainoa oikea tapa on lisätä laitteelta lähtevän kiertoilman lämpötilaa tarpeen mukaan. Nyt maalämmityksen kanssa tulee nähtäväksi miten usein siltä vaaditaan noita korkeita veden lämpötiloja (laitteisto turvautuu silloin yleensä sähkövastuksen apuun). Hyötysuhteen kannalta olisi ilmeisesti parempi nostaa kiertoilman nopeutta, mutta saa nähdä.</Container>
