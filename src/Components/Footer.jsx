e627d862e5f48e607be5616a24b4882dbff85941
Olli-Pekka Wallin <olli-pekka.wallin@wallin.biz>
2020-05-12 12:29:21 +0300

    typo_ remove dot from end of copyright message

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ import React from 'react'
 
 const Footer = () =>
     <div className="ui basic center aligned segment" >
-        Olli-Pekka Wallin (c) 2013 - 2020.
+        Olli-Pekka Wallin (c) 2013 - 2020
         <p>  info (at) ilmalammitys dot net </p>
     </div>
 export default Footer
