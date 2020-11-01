56f7efc388efdf172db032bbf0157311808703f2
Olli-Pekka Wallin <olli-pekka.wallin@wallin.biz>
2020-11-01 21:19:46 +0200

    Add label to include version inforamtion

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,15 +2,17 @@
  * Copyright (c) 2020.  Olli-Pekka Wallin All rights reserved
  */
 import React from 'react'
+import ChangeHistory from "../Pages/ChangeHistory/ChangeHistory";
 
 const Footer = () =>
     <div className="ui basic center aligned segment" >
-        Olli-Pekka Wallin (c) 2013 - 2020
+        <ChangeHistory/> Olli-Pekka Wallin (c) 2013 - 2020
         <p>
-            info (at) ilmalammitys dot net <br/>
-             v1.10 10.10.2020    
-           </p>
+            info (at) ilmalammitys dot net  <br/>
+        </p>
+        <div> <p></p></div>
     </div>
+
 export default Footer
 
 
--- src/Pages/ChangeHistory/ChangeHistory.jsx
+++ src/Pages/ChangeHistory/ChangeHistory.jsx
@@ -3,10 +3,27 @@
  */
 
 import React from "react";
+import {Popup,Label} from "semantic-ui-react";
 
-const ChangeHistory = ({text, url}) =>
-    <a href={url}>
-        {text}
-    </a>;
+const history = [
+    {
+        version: '1.10',
+        reason: 'Lisätty Lämpösampö LS-4000 ilmälämmityslaitteiston pdf tiedostot',
+        date: '15.10.2020'
+    }
+]
+
+const ChangeHistory = () => (
+    <>
+        <Popup
+            content={history[0].reason}
+            key={history[0].date}
+            header={history[0].version}
+            trigger={  <Label as='a'>
+                Versio 1.10</Label>
+            }
+        />
+    </>
+)
 
 export default ChangeHistory
