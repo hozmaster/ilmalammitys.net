4756f7a11fc73a5ce0e11aab0a8043085bf74f82
Olli-Pekka Wallin <olli-pekka.wallin@wallin.biz>
2020-12-06 21:03:12 +0200

    Finalize the ChangeHistory-component

--- src/Pages/ChangeHistory/ChangeHistory.jsx
+++ src/Pages/ChangeHistory/ChangeHistory.jsx
@@ -3,23 +3,26 @@
  */
 
 import React from "react";
-import {Popup,Label} from "semantic-ui-react";
+import {Popup} from "semantic-ui-react";
 
 const history = [
     {
         version: '1.10',
-        reason: 'Lisätty Lämpösampö LS-4000 ilmälämmityslaitteiston pdf tiedostot',
+        reason: 'Lisätty Lämpösampö LS-4000',
         date: '15.10.2020'
     }
 ]
 
+const the_link = "https://github.com/hozmaster/ilmalammitys.net"
+
 const ChangeHistory = () => (
     <>
+        Versio : 
         <Popup
             content={history[0].reason}
             key={history[0].version}
             header={history[0].date}
-            trigger={  <a href={'#'} > v1.10 </a>
+            trigger={  <a href={ the_link } > {history[0].version} </a>
             }
         />
     </>
